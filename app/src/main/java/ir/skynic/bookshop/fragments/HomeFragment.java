6b611fe2504b8af5ec24c3d047d978ac84d4bbab
Ibrahum93 <ce.ibrahim93@gmail.com>
2020-07-12 15:39:27 +0430

    test commit

--- app/src/main/java/ir/skynic/bookshop/fragments/HomeFragment.java
+++ app/src/main/java/ir/skynic/bookshop/fragments/HomeFragment.java
@@ -1,20 +1,30 @@
 package ir.skynic.bookshop.fragments;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.List;
+import java.util.Map;
 
 import ir.skynic.bookshop.Configuration;
+import ir.skynic.bookshop.Utils;
 import ir.skynic.bookshop.activities.MainActivity;
 import ir.skynic.bookshop.R;
 import ir.skynic.bookshop.api.ApiClient;
 import ir.skynic.bookshop.model.Book;
+import ir.skynic.bookshop.model.User;
 import ir.skynic.bookshop.view.BookView;
+import ir.skynic.bookshop.view.CategoryView;
+import ir.skynic.bookshop.view.UserView;
 
 
 public class HomeFragment extends Fragment {
@@ -25,6 +35,8 @@ public class HomeFragment extends Fragment {
     private ViewGroup userContainer;
     private ViewGroup newProductContainer;
     private ViewGroup categoryContainer;
+
+    private ImageView imageView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -46,6 +58,46 @@ public class HomeFragment extends Fragment {
     }
 
     private void getInformation() {
+        new Thread(() -> {
+            try {
+                String viewRequest[] = {"get-view", Configuration.getUsername(getActivity())};
+                String jsonContent = ApiClient.getJson(viewRequest);
+
+                if(jsonContent != null) {
+                    try {
+                        JSONObject jsonObject = new JSONObject(jsonContent);
+                        String error = jsonObject.getString("error");
+
+                        if(error.equals("0")) {
+                            String view = jsonObject.getJSONArray("view").getJSONObject(0).optString("viewLink");
+
+                            Bitmap bitmap = Utils.getImageFromUrl(view);
+                            Utils.runOnMainThread(() -> imageView.setImageBitmap(bitmap));
+                        }
+                    } catch (Exception ignore) { }
+                }
+
+            } catch (Exception ignored) {}
+        }).start();
+
+
+//        ApiClient.getModel(viewRequest, "view", Book.class, o -> {
+//            if(o != null) {
+//
+//                List<Book> bookList = (List) o[1];
+//
+//                new Thread(() -> {
+//                    try {
+//                        Bitmap bitmap = Utils.getImageFromUrl(model.getThumbnailImageLink());
+//                        Utils.runOnMainThread(() -> imageView.setImageBitmap(bitmap));
+//                    } catch (Exception ignored) {}
+//                }).start();
+//
+//            } else {
+//                Toast.makeText(getActivity(), "خطایی پیش آمد... لطفا دوباره امتحان کنید.", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
 
         String disCountedRequest[] = {"get-dis-counted", Configuration.getUsername(getActivity()), "30"};
         ApiClient.getModel(disCountedRequest, "book", Book.class, o -> {
@@ -61,6 +113,26 @@ public class HomeFragment extends Fragment {
                 }
 
                 productContainer.setVisibility(View.VISIBLE);
+
+            } else {
+                Toast.makeText(getActivity(), "خطایی پیش آمد... لطفا دوباره امتحان کنید.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        String userRequest[] = {"get-top-seller", Configuration.getUsername(getActivity()), "30"};
+        ApiClient.getModel(userRequest, "user", User.class, o -> {
+            if(o != null) {
+
+                List<User> userList = (List) o[1];
+
+                userContainer.removeAllViews();
+
+                for (User user : userList) {
+                    UserView userView = new UserView(getActivity(), UserView.ViewSize.SMALL, user);
+                    userContainer.addView(userView);
+                }
+
+                userContainer.setVisibility(View.VISIBLE);
 
             } else {
                 Toast.makeText(getActivity(), "خطایی پیش آمد... لطفا دوباره امتحان کنید.", Toast.LENGTH_SHORT).show();
@@ -84,16 +156,42 @@ public class HomeFragment extends Fragment {
                 Toast.makeText(getActivity(), "خطایی پیش آمد... لطفا دوباره امتحان کنید.", Toast.LENGTH_SHORT).show();
             }
         });
+
+        Map categories = Configuration.getCategories();
+        categoryContainer.removeAllViews();
+        for (Object o : categories.keySet()) {
+            int key = (int) o;
+            String value = (String) categories.get(key);
+
+            CategoryView categoryView = new CategoryView(getActivity(), CategoryView.ViewSize.SMALL, value);
+            categoryView.setOnClickListener(view -> {
+
+                SearchFragment searchFragment = new SearchFragment();
+                searchFragment.clearFilters();
+                searchFragment.setCategory(key);
+
+                MainActivity.showFragment(HomeFragment.this.getActivity(), searchFragment);
+            });
+
+            categoryContainer.addView(categoryView);
+        }
+
+        categoryContainer.setVisibility(View.VISIBLE);
     }
 
     private void initUi() {
+        imageView = mView.findViewById(R.id.imageView);
+
         productContainer = mView.findViewById(R.id.lnrProductContainer);
         productContainer.setVisibility(View.GONE);
+
         userContainer = mView.findViewById(R.id.lnrUserContainer);
-//        userContainer.setVisibility(View.GONE);
+        userContainer.setVisibility(View.GONE);
+
         newProductContainer = mView.findViewById(R.id.lnrNewProductContainer);
         newProductContainer.setVisibility(View.GONE);
+
         categoryContainer = mView.findViewById(R.id.lnrCategoryContainer);
-//        categoryContainer.setVisibility(View.GONE);
+        categoryContainer.setVisibility(View.GONE);
     }
 }
